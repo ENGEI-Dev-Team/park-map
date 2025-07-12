cca2d9ce393d32eb2a58ab537a4ec978b4dd903d
鈴木一世 <woodsuzu1260@gmail.com>
2025-07-12 12:57:43 +0900

    データを更新

--- app/contents/park-item/index.ts
+++ app/contents/park-item/index.ts
@@ -1,1 +1,4 @@
 // 再エクスポート用
+
+export { type Facility, type Category } from "./type";
+export { facilitiesData } from "./data";
--- app/contents/park-item/type.ts
+++ app/contents/park-item/type.ts
@@ -1,1 +1,14 @@
 // 型定義（Facility, FacilityCategory）
+// 型定義（Facility, FacilityCategory）
+
+export type Facility = {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+};
+
+export type Category = {
+  category: string;
+  items: Facility[];
+};
